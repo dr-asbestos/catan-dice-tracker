2360c1e928821b815ee6a9310b5c385cbda98445
dr-asbestos <egor.khramenko@mail.ru>
2026-08-03 16:52:11 +1200

    remove garbage

--- utils/tracker.py
+++ utils/tracker.py
@@ -29,8 +29,9 @@ class Tracker:
         self.rollDiffs = {k:sampleRolls.get(k, 0) - v for k,v in self.idealRolls.items()}
         self.luckDiffs = {k:v/len(self.rolls) for k,v in self.rollDiffs.items()}
 
-        self.formatted = {k:f'{v:+.2%}' for k,v in self.luckDiffs.items()}
-        pprint(self.formatted)
+        #self.formatted = {k:f'{v:+.2%}' for k,v in self.luckDiffs.items()}
+        #pprint(self.formatted)
+
     
     def newRoll(self, roll):
         '''
@@ -44,28 +45,4 @@ class Tracker:
         else:
             self.rolls.append(int(roll))
             self.calculate()
-
     
-
-'''
-# rolling paramenters
-nRolls = 360
-diceCombs = {2:1, 3:2, 4:3, 5:4, 6:5, 7:6, 8:5, 9:4, 10:3, 11:2, 12:1}
-# derived ideal stats
-totalCombs = sum(diceCombs.values())
-idealRolls = {k:v/totalCombs*nRolls for k,v in diceCombs.items()}
-print(idealRolls)
-
-# generate random dice rolls
-sampleRolls = Counter(choices(list(diceCombs.keys()), diceCombs.values(), k=nRolls))
-print(sampleRolls)
-# number of rolls differences between ideal and actual rolls
-rollDiffs = {k:sampleRolls.get(k, 0) - v for k,v in idealRolls.items()}
-pprint(rollDiffs)
-# same but ratio form
-luckDiffs = {k:v/nRolls for k,v in rollDiffs.items()}
-pprint(luckDiffs)
-# pretty print
-formatted = {k:f'{v:+.2%}' for k,v in luckDiffs.items()}
-pprint(formatted)
-'''
